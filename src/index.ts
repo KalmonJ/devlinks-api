9418160fae9d9fd97546a6217cf17b0537e16a9d
KalmonJ <kalmonkk69@gmail.com>
2023-09-06 00:33:34 -0300

    feat: env

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { userRoute } from "./infra/http/routes/UserRoutes";
 import { authRoute } from "./infra/http/routes/AuthRoutes";
 import { dbConnection } from "./infra/db/mongodb/connection";
+import { config } from "dotenv";
+config();
 
 dbConnection;
 const app = express();
-const PORT = 3030;
+const PORT = process.env.PORT ?? 3333;
 
 app.use(express.json());
 app.use(userRoute, authRoute);
