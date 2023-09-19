a6d7ddd6408e4fe8c7a831108ac5f8fdca05856b
KalmonJ <kalmonkk69@gmail.com>
2023-09-19 12:16:24 -0300

    fix: session return

--- src/infra/controllers/AuthController.ts
+++ src/infra/controllers/AuthController.ts
@@ -28,7 +28,6 @@ export class AuthController {
       .status(200)
       .cookie("session", response.value, {
         httpOnly: true,
-        domain: "http://localhost:3000",
         sameSite: "none",
       })
       .send({});
